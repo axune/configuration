d83bd0a3708396a2b46d295e82e6d0fd4a818a4d
Ben Radford <axune86@gmail.com>
2014-04-25 20:17:44 -0700

    Changed filename format of backup file link.sh creates.

--- link.sh
+++ link.sh
@@ -7,7 +7,7 @@ SCRIPT=$(basename $0)
 for FILE in $(ls $HOME/$CONFIG_DIR | grep -v $SCRIPT); do
     if [ "(" -e "$HOME/.$FILE" ")" -a "(" ! -h "$HOME/.$FILE" ")" ]; then
         echo -n "moving existing config '$FILE' out of the way first: "
-        mv -v $HOME/.$FILE $HOME/config_backup_$DATE-$FILE
+        mv -v $HOME/.$FILE "$HOME/config_backup_$DATE:$FILE"
     fi
     ln -svfT $CONFIG_DIR/$FILE $HOME/.$FILE
 done
